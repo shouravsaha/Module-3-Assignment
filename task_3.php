a2b664ba42f87dc8f9a2632bff862a10ca6bb0e5
Shourav Saha <shourav939@gmail.com>
2023-09-26 20:38:20 +0600

    task-3 completed

---
+++ task_3.php
@@ -0,0 +1,13 @@
+<?php
+
+$grades = array(85, 92, 78, 88, 95);
+
+function sortGradesDescending($grades) {
+    arsort($grades); 
+    return $grades;
+}
+
+$sortedGrades = sortGradesDescending($grades);
+
+print_r($sortedGrades);
+?>
